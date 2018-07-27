da1a656cc5cd026194c854d452d33bad20129018
imran <imufun1@gmail.com>
2018-07-28 00:34:19 +0600

    init

---
+++ app/src/main/java/com/weathertoday/sinbad/weathertoday/Model/WeatherResult.java
@@ -0,0 +1,20 @@
+package com.weathertoday.sinbad.weathertoday.Model;
+
+import java.util.List;
+
+public class WeatherResult {
+
+
+    private Coord coord;
+    private List<Weather> weather;
+    private String base;
+    private Main main;
+    private int visibility;
+    private Wind wind;
+    private Clouds clouds;
+    private int dt;
+    private Sys sys;
+    private int id;
+    private String name;
+    private int cod;
+}
---
+++ app/src/main/java/com/weathertoday/sinbad/weathertoday/common/Common.java
@@ -0,0 +1,7 @@
+package com.weathertoday.sinbad.weathertoday.common;
+
+public class Common {
+    public static final String APP_ID = "f279df88f01232850ddff621c125603d";
+   // public static final String BASE_URL = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=";
+
+}
