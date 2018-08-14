bb1cdf18e22ec05bdbf0255eddbc7ac62d2fd6a5
imran <imufun1@gmail.com>
2018-08-14 20:48:14 +0600

    update 5days weather forecast: -add model -refactor

---
+++ app/src/main/java/com/weathertoday/sinbad/weathertoday/Model/weather/Clouds.java
@@ -0,0 +1,16 @@
+package com.weathertoday.sinbad.weathertoday.Model.weather;
+
+public class Clouds {
+    private int all;
+
+    public Clouds() {
+    }
+
+    public int getAll() {
+        return all;
+    }
+
+    public void setAll(int all) {
+        this.all = all;
+    }
+}
